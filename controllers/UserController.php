0f30361d15745f348169d1a9693d378e69d6d186
Evg <dreammago@gmail.com>
2019-07-18 09:57:48 +0400

    ttt

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -40,7 +40,7 @@ class UserController extends Controller
 
         return $this->render('index', [
             'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
+            'dataProvider' => $dataProvider
         ]);
     }
 
