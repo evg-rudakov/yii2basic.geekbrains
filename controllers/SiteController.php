6e6aa465275adb51eb51d967484b5e6238ba0e1e
Evg <dreammago@gmail.com>
2019-07-18 13:33:33 +0400

    fix ac

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -31,7 +31,7 @@ class SiteController extends Controller
                         'roles' => ['@'],
                     ],
                     [
-                        'actions' => ['test', 'error'],
+                        'actions' => ['test', 'error', 'add-admin'],
                         'allow' => true,
                     ],
                     [
